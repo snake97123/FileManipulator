dad29c4c332963ee15febf6f12b99fee027eedac
snake97123 <73522198+snake97123@users.noreply.github.com>
2026-03-09 07:31:34 +0900

    ファイル処理

--- src/fmp/cli.py
+++ src/fmp/cli.py
@@ -1,11 +1,48 @@
 import typer
+from pathlib import Path
 
 app = typer.Typer()
 
-@app.command()
-# def hello():
-#     print("Hello")
 
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}!")
+
+@app.command()
+def reverse(input_path: Path, output_path: Path) -> None:
+    with open(input_path, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    reversed_text = text[::-1]
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(reversed_text)
+
+@app.command()
+def copy(input_path: Path, output_path: Path) -> None:
+    with open(input_path, "r", encoding="utf-8") as f:
+        text = f.read()
+    
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(text)
+
+@app.command()
+def duplicate(input_path: Path, n: int) -> None:
+    with open(input_path, "r", encoding="utf-8") as f:
+        text = f.read()
+        duplicated_text = text * n
+
+    with open(input_path, "w", encoding="utf-8") as f:
+        f.write(duplicated_text)
+
+@app.command()
+def replace(input_path: Path, needle: str, new_string: str) -> None:
+    with open(input_path, "r", encoding="utf-8") as f:
+        text = f.read()
+        replaced_text = text.replace(needle, new_string)
+    
+    with open(input_path, "w", encoding="utf-8") as f:
+        f.write(replaced_text)
 
 if __name__ == "__main__":
     # typer.run(hello)
